a0970182f98509322299e4832c007c01322ddc40
An Thien Tran <tranthienan.90@gmail.com>
2019-02-23 18:38:38 +0700

    Fixed lyrics fetching

--- src/services/lyrics.service.ts
+++ src/services/lyrics.service.ts
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
 function getLyrics(songName: string, artistName: string) {
-  // just get the song's actual name
-  const indexOfParen = songName.indexOf('(');
-  const endPos = indexOfParen === -1 ? songName.length : indexOfParen;
-  const name = songName.substring(0, endPos);
   return axios
     .get('/api/maeve-lyrics', {
       params: {
-        songName: name.trim(),
+        songName: songName.trim(),
         artistName: artistName.trim()
       }
     })
